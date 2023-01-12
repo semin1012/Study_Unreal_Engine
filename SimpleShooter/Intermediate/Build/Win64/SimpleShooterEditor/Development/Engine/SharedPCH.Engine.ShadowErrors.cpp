3201221963a1047e821c73ed1a3c03d5c6d3f843
semin1012 <73764069+semin1012@users.noreply.github.com>
2023-01-12 23:23:42 +0900

    add project Section 6

---
+++ SimpleShooter/Intermediate/Build/Win64/SimpleShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31629
+#include "D:/00.Project/00.Study/Study_Unreal_Engine/SimpleShooter/Intermediate/Build/Win64/SimpleShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ SimpleShooter/Intermediate/Build/Win64/UnrealEditor/Inc/SimpleShooter/ShooterCharacter2.generated.h
@@ -0,0 +1,90 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SIMPLESHOOTER_ShooterCharacter2_generated_h
+#error "ShooterCharacter2.generated.h already included, missing '#pragma once' in ShooterCharacter2.h"
+#endif
+#define SIMPLESHOOTER_ShooterCharacter2_generated_h
+
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_SPARSE_DATA
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_RPC_WRAPPERS
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAShooterCharacter2(); \
+	friend struct Z_Construct_UClass_AShooterCharacter2_Statics; \
+public: \
+	DECLARE_CLASS(AShooterCharacter2, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SimpleShooter"), NO_API) \
+	DECLARE_SERIALIZER(AShooterCharacter2)
+
+
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAShooterCharacter2(); \
+	friend struct Z_Construct_UClass_AShooterCharacter2_Statics; \
+public: \
+	DECLARE_CLASS(AShooterCharacter2, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SimpleShooter"), NO_API) \
+	DECLARE_SERIALIZER(AShooterCharacter2)
+
+
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AShooterCharacter2(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AShooterCharacter2) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AShooterCharacter2); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AShooterCharacter2); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AShooterCharacter2(AShooterCharacter2&&); \
+	NO_API AShooterCharacter2(const AShooterCharacter2&); \
+public:
+
+
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AShooterCharacter2(AShooterCharacter2&&); \
+	NO_API AShooterCharacter2(const AShooterCharacter2&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AShooterCharacter2); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AShooterCharacter2); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AShooterCharacter2)
+
+
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_9_PROLOG
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_SPARSE_DATA \
+	FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_RPC_WRAPPERS \
+	FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_INCLASS \
+	FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_SPARSE_DATA \
+	FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_INCLASS_NO_PURE_DECLS \
+	FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SIMPLESHOOTER_API UClass* StaticClass<class AShooterCharacter2>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_SimpleShooter_Source_SimpleShooter_ShooterCharacter2_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
